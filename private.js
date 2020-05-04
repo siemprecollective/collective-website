5098c49e2d0755a45a7bc9c2756e90a32143a8f1
Cyrus Tabrizi <cyrus.tabrizi@gmail.com>
2020-05-04 01:20:58 -0700

    updated Collective website

--- private.js
+++
@@ -1,9 +0,0 @@
-window.onload = (e) => {
-  let disableButton = document.getElementById("disable-button");
-  disableButton.onclick = (e) => {
-    createCookie("disableAnalytics", "true", 3000);
-    let parentEl = disableButton.parentNode;
-    parentEl.removeChild(disableButton);
-    parentEl.appendChild(htmlToElement("<div>Disabled!</div>"));
-  }
-}
